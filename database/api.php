26eb181b4ba0cabb6869a0fdde976d34c99d1cc6
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-08-24 15:44:34 +0200

    ADD json database and convert to array

--- database/api.php
+++ database/api.php
@@ -1,34 +1,7 @@
 <?php 
 
-$disks = [
-    [
-        'title' => 'Thriller',
-        'author' => 'Michael Jackson',
-        'year' => '1982'
-    ],
-    [
-        'title' =>  'Justified',
-        'author' => 'Justin Timberlake',
-        'year' =>   '2002'
-    ],
-    [
-        'title' => 'The Fame',
-        'author' => 'Lady Gaga',
-        'year' =>   '2008'
-    ],
-    [
-        'title' => 'One of the Boys',
-        'author' => 'Katy Perry',
-        'year' =>   '2009'
-    ],
-    [
-        'title' =>  'Lemonade',
-        'author' => 'Beyoncè',
-        'year' =>   '2016'
-    ],
-    [
-        'title' =>  '1989',
-        'author' => 'Taylor Swift',
-        'year' =>   '2014'
-    ]
-];
+$string = file_get_contents('disks.json');
+
+$disks = json_decode($string, true);
+
+var_dump($disks);
