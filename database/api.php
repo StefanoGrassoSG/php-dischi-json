22155ba5aca7dd50da06de9197688d71a708343a
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-08-24 15:20:03 +0200

    ADD basic structure and api.php

---
+++ database/api.php
@@ -0,0 +1,34 @@
+<?php 
+
+$disks = [
+    [
+        'title' => 'Thriller',
+        'author' => 'Michael Jackson',
+        'year' => '1982'
+    ],
+    [
+        'title' =>  'Justified',
+        'author' => 'Justin Timberlake',
+        'year' =>   '2002'
+    ],
+    [
+        'title' => 'The Fame',
+        'author' => 'Lady Gaga',
+        'year' =>   '2008'
+    ],
+    [
+        'title' => 'One of the Boys',
+        'author' => 'Katy Perry',
+        'year' =>   '2009'
+    ],
+    [
+        'title' =>  'Lemonade',
+        'author' => 'Beyoncè',
+        'year' =>   '2016'
+    ],
+    [
+        'title' =>  '1989',
+        'author' => 'Taylor Swift',
+        'year' =>   '2014'
+    ]
+];
---
+++ js/script.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue
+
+    createApp({
+        data() {
+            return {
+                message: 'PORCO DIO'
+            }
+        },
+        created() {
+            
+        }
+    }).mount('#app')
